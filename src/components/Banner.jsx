b8b91e84e5474eaf3efda08af06572debd66bcd1
Muhammed Adham <muharaafat@gmail.com>
2025-07-13 00:49:47 +0300

    Banner__AlmostThere

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import "./Banner.css"
-import AnnouncementBar from './AnnouncementBar'
-import LoaderPage from './LoaderPage'
+import React, { useState, useEffect } from "react";
+import "./Banner.css";
+import AnnouncementBar from "./AnnouncementBar";
+import LoaderPage from "./LoaderPage";
 
-const imagesToPreload = [
-  '/loud-6.jpg',
-  '/saxo.png',
-];
+const imagesToPreload = ["/loud-6.jpg", "/saxo.png"];
 
 const Banner = () => {
   const [imagesLoaded, setImagesLoaded] = useState(false);
@@ -31,14 +28,23 @@ const Banner = () => {
 
   return (
     <>
-      <div className='banner__container'>
-        <img className="banner__background-img" src="/loud-6.jpg" alt="Banner background" />
-        <div className="banner__container-img">
-          <img src="/saxo.png" alt="" />
+      <div className="banner__container">
+        <div className="banner__container-slogan">
+          <h2>Egypt's Saxophone</h2>
         </div>
-      
+        <img
+          className="banner__background-img"
+          src="/sugar.jpeg"
+          alt="Banner background"
+        />
+        <div className="banner__container-img">
+          <img src="/saxo-2.png" alt="" />
+        </div>
         <div className="banner__container-content">
-          <div className="banner__container-title"> <h2>Join The Hub</h2></div>
+          <div className="banner__container-title">
+            {" "}
+            <h2>Join The Hub</h2>
+          </div>
           <div className="banner__container-cards">
             {/* <div className="banner__card"><img src="./loud.jpg" alt="" /></div> */}
             {/* <div className="banner__card"><img src="./loud.jpg" alt="" /></div> */}
@@ -48,17 +54,18 @@ const Banner = () => {
             <p>
               <b>More Than Just Accessories</b>
               <br />
-              We believe that great music starts with great gear, but it thrives through great connections. We're not just selling saxophone accessories – we're cultivating Egypt's most vibrant saxophone community, one player at a time.
+              We believe that great music starts with great gear, but it thrives
+              through great connections. We're not just selling saxophone
+              accessories – we're cultivating Egypt's most vibrant saxophone
+              community, one player at a time.
             </p>
           </div>
         </div>
-        <div className="banner__container-circle">
-
-        </div>
+        <div className="banner__container-circle"></div>
         <AnnouncementBar announcemen={"Free Shipping for Nasr City"} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
